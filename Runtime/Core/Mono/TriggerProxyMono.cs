3ad66fe923aec776b620a6331f32150680759b09
Patryk Piotrowski <patryk.piotrowski@lucidrealitylabs.com>
2026-02-15 16:47:04 +0100

    Added `TriggerProxyMono`

---
+++ Runtime/Core/Mono/TriggerProxyMono.cs
@@ -0,0 +1,63 @@
+﻿#region
+
+using Cysharp.Threading.Tasks;
+using Dev.Cortez.StateMachines.Core.Interfaces;
+using Dev.Cortez.StateMachines.Core.ReferencePicker.Trigger;
+using UnityEngine;
+using UnityEngine.Events;
+
+#endregion
+
+namespace Dev.Cortez.StateMachines.Core.Mono
+{
+    public sealed class TriggerProxyMono : MonoBehaviour
+    {
+        [SerializeField]
+        private TriggerReferencePicker _triggerReferencePicker;
+
+        public UnityEvent<bool> triggeredValueChanged;
+
+        private ITrigger _trigger;
+
+        private void Awake()
+        {
+            _triggerReferencePicker.Observe(OnTriggerResolved, OnTriggerUnresolved, destroyCancellationToken);
+        }
+
+        private void OnDestroy()
+        {
+            DetachTrigger();
+        }
+
+        private void OnTriggerUnresolved()
+        {
+            DetachTrigger();
+        }
+
+        private void DetachTrigger()
+        {
+            if (_trigger != null)
+            {
+                _trigger.TriggeredValueChanged -= OnTriggeredValueChanged;
+                _trigger = null;
+            }
+        }
+
+        public void TriggerValue(bool value)
+        {
+            _triggerReferencePicker.ResolveReferenceAsync(destroyCancellationToken).ContinueWith(trigger =>
+                trigger.TriggerValueAsync(value, destroyCancellationToken).Forget()).Forget();
+        }
+
+        private void OnTriggerResolved(ITrigger trigger)
+        {
+            _trigger = trigger;
+            _trigger.TriggeredValueChanged += OnTriggeredValueChanged;
+        }
+
+        private void OnTriggeredValueChanged(ITrigger _, bool value)
+        {
+            triggeredValueChanged?.Invoke(value);
+        }
+    }
+}
